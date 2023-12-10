f0ab94389f8ccdd31f7c624411435dad8ca9f34b
RDH <tsoa.le.rdh@gmail.com>
2023-12-10 11:31:56 +0300

    remove axios

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import axios from "axios";
 import Channel from "./Channel";
 import { Options } from "./types";
 import io, { Socket } from 'socket.io-client'; 
@@ -7,10 +6,8 @@ export default class RdhClientSocket {
     private options: Options;
     private ws_host: string;
     private socket: Socket;
-    private api_url: string;
-
+$
     constructor(options: Options) {
-        this.api_url = "https://rdh-socket.wuaze.com/api/subscribe"
 
         if (!options.app_ws) {
             this.ws_host = "https://rdh-websocket.onrender.com"
@@ -23,25 +20,9 @@ export default class RdhClientSocket {
     }
 
     async init() {
-        const status = await this.verifyToken()
-        this.options.status = status;
         this.socket = io(this.ws_host, {
             query: this.options
         });
-
-        if (!status) {
-            console.error("Invalid TOKEN");
-        }
-    }
-
-    async verifyToken() {
-        const fullURL = this.api_url+`?app_id=${this.options.app_id}&app_key=${this.options.app_key}`
-
-        return await axios.get(fullURL).then(response => {
-            return response.data.status;
-        }).catch(() => {
-            return false;
-        })
     }
 
     channel(channel_name: string): Channel {
--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -4,5 +4,4 @@ export type Options = {
   app_id: string;
   app_ws: string | null | undefined; 
   app_key: string;
-  status: boolean | false;
 };
